444d0cca0c26ee972cff0143d72ff6ee9b79faaa
mcdoggy09 <antid@DESKTOP-21N1S06>
2025-01-09 22:23:33 -0600

    delete

--- src/main/java/net/mcreator/dreamlike/DreamlikeMod.java
+++ src/main/java/net/mcreator/dreamlike/DreamlikeMod.java
@@ -17,8 +17,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
-import net.mcreator.dreamlike.init.DreamlikeModItems;
-
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -38,8 +36,6 @@ public class DreamlikeMod {
 		// End of user code block mod constructor
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-		DreamlikeModItems.REGISTRY.register(bus);
 
 		// Start of user code block mod init
 		// End of user code block mod init
